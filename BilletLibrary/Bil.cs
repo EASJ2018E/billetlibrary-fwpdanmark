8d0015b7fc710b7f759beedc10e270b3edd2aae7
Frederik Wulff Pedersen <fred957b@edu.easj.dk>
2018-09-18 12:19:10 +0200

    Opgave 1+2

---
+++ BilletLibrary/Bil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BilletLibrary
+{
+    public class Bil
+    {
+
+        public string NummerPlade { get; set; }
+
+        public DateTime Dato { get; set; }
+        /// <summary>
+        /// Retunerer pris som i denne er 240
+        /// </summary>
+        /// <returns></returns>
+        public decimal Pris()
+        {
+            return 240;
+        }
+        /// <summary>
+        /// Returnerer køretøjtype som i denne er Bil
+        /// </summary>
+        /// <returns></returns>
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+
+    }
+}
